0341fad376f3074540174b0096f386028920d240
alejcuevas97 <alejcuevas97@gmail.com>
2026-03-28 04:28:34 -0400

    cambios requirements

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -95,10 +95,7 @@ WSGI_APPLICATION = 'Portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL')
 }
 
 
