4040b4c1db9bc9f6d4d6cc43550d7eaac2ccf672
alejcuevas97 <alejcuevas97@gmail.com>
2026-03-28 05:37:40 -0400

    cambios

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -134,19 +134,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
-
-
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= BASE_DIR, 'staticfiles'
 
 
 #archivos subidos desde la apk
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR, 'media'
 
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    
 #registrando la apk "theme"
 TAILWIND_APP_NAME = "theme"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
