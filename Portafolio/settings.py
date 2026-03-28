2836047cf8dbb2bb632b6bd2edd36db36b44e7a6
alejcuevas97 <alejcuevas97@gmail.com>
2026-03-28 05:43:07 -0400

    1

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
