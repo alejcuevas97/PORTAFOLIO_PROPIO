8fc19d7658ecec2d79e8d3b430b75407940ab8f2
alejcuevas97 <alejcuevas97@gmail.com>
2026-03-29 15:27:06 -0400

    ROOT

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
-STATIC_ROOT = [
+ROOT = [
     os.path.join(BASE_DIR, 'static_root')]
 
 STATICFILES_DIRS = [
