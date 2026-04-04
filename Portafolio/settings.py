6846573934a3ede0acb41506fed759112dd5ad9b
alejcuevas97 <alejcuevas97@gmail.com>
2026-04-04 11:52:29 -0400

    2

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -141,10 +141,11 @@ USE_TZ = True
 
 # Archivos estáticos
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Archivos de usuario (media)
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join( BASE_DIR / "media" ) 
 
